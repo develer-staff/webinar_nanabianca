beff0fa3563aa9a684312c538755bf911aeed2ff
Alessandro <alessandro.mamusa@nextroll.com>
2023-11-25 15:08:11 +0100

    style

--- test_grid.py
+++ test_grid.py
@@ -1,32 +1,36 @@
-import grid
 import numpy as np
+
+import grid
+
 
 def test_HandleMouse():
     scale = 1
     w, h = 10, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
     g.grid_array.fill(0)
 
     x, y = 5, 5
-    g.HandleMouse(x,y)
+    g.HandleMouse(x, y)
 
     assert g.grid_array[x][y] == 1
+
 
 def test_NumColsRows():
     scale = 2
     w, h = 20, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
 
     assert g.columns == 5
     assert g.rows == 10
+
 
 def test_evolve_empty():
     scale = 1
     w, h = 10, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
     g.grid_array.fill(0)
 
     next = np.ndarray(shape=(g.size))
@@ -35,11 +39,12 @@ def test_evolve_empty():
     # No evolution from empty grid
     assert np.array_equal(next, g.grid_array)
 
+
 def test_underpopulation_one_cell():
     scale = 1
     w, h = 10, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
     g.grid_array.fill(0)
 
     g.grid_array[5][5] = 1
@@ -53,19 +58,20 @@ def test_underpopulation_one_cell():
     # Underpopulation: Single cell dies
     assert g.grid_array[5][5] == 0
 
+
 def test_overpopulation_one_cell():
     scale = 1
     w, h = 10, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
     g.grid_array.fill(0)
 
     g.grid_array[5][5] = 1
 
     # Neighbours
-    nn = [(4,5), (6,5), (5,4), (5,6)]
+    nn = [(4, 5), (6, 5), (5, 4), (5, 6)]
     for n in nn:
-        x,y = n
+        x, y = n
         g.grid_array[x][y] = 1
 
     assert g.get_neighbours(5, 5) == 4
@@ -76,18 +82,19 @@ def test_overpopulation_one_cell():
     # Overpopulation: Single cell dies
     assert g.grid_array[5][5] == 0
 
+
 def test_reproduction_one_cell():
     scale = 1
     w, h = 10, 10
 
-    g = grid.Grid(w,h, scale, 0)
+    g = grid.Grid(w, h, scale, 0)
     g.grid_array.fill(0)
     g.grid_array[5][5] = 0
 
     # Neighbours
-    nn = [(4,5), (6,5), (5,4)]
+    nn = [(4, 5), (6, 5), (5, 4)]
     for n in nn:
-        x,y = n
+        x, y = n
         g.grid_array[x][y] = 1
 
     assert g.get_neighbours(5, 5) == 3
